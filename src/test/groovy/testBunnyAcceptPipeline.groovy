68a2245403e4a29f470e37febbaa085fcd558b83
Christoph Henrici <chhenrici@gmail.com>
2020-05-14 19:27:13 +0200

    Debugging puts

--- src/test/groovy/testBunnyAcceptPipeline.groovy
+++ src/test/groovy/testBunnyAcceptPipeline.groovy
@@ -2,5 +2,5 @@
 // Invoking external Ruby Script , which waits blocking for subscription
 node {
     recievedMsg = sh(returnStdout: true, script: "$SCRIPTS_DIR/input.rb").trim()
-    echo "Input Script output: $recievedMsg"
+        echo "Input Script output: $recievedMsg"
 }
