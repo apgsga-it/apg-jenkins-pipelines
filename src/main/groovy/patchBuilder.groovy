507ea7e26dbee7ba1facc603d707f52d47024a40
Christoph Henrici <chhenrici@gmail.com>
2020-09-16 17:47:34 +0200

    Test Script Permissive Approval

--- src/main/groovy/patchBuilder.groovy
+++ src/main/groovy/patchBuilder.groovy
@@ -2,7 +2,6 @@ import hudson.model.*
 def patchName = "Patch${patchnumber}"
 def jobName = patchName
 def downLoadJobName = jobName + "OnDemand"
-// Test Permissive Script Approval, some more
 pipelineJob (jobName) {
 	authenticationToken(patchName)
 	concurrentBuild(false)
