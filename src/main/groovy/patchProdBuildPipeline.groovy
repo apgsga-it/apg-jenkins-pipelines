4bf87aedc35c6f618f10174bc3a9bc3f71338bc4
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-09-29 10:26:54 +0200

    very basic structure in place

--- src/main/groovy/patchProdBuildPipeline.groovy
+++ src/main/groovy/patchProdBuildPipeline.groovy
@@ -1,33 +1,25 @@
 #!groovy
-library 'patch-global-functions'
-#! WORK IN PROGRESS NOT READY FOR TESTING
-#! Test Permissive Script Approval
-properties([
-	parameters([
-		stringParam(
-		defaultValue: "",
-		description: 'Path to Patch*.json File',
-		name: 'PARAMETER'
-		),
-		stringParam(
-		defaultValue: "FALSE",
-		description: 'Indicator, if the Pipeline should be restartet to the last successful state',
-		name: 'RESTART'
-		)
-	])
-])
 
-def patchConfig = commonPatchFuntions.readPatchFile(params.PARAMETER)
-commonPatchFuntions.initPatchConfig(patchConfig,params)
-println "PatchConfig:"
-println patchConfig.toString()
-//
-// Load Target System Mappings
-def targetSystemsMap = commonPatchFuntions.loadTargetsMap()
-commonPatchFuntions.log("TargetSystemsMap : ${targetSystemsMap} ")
-// Create a local Maven Repo for Pipeline
-// Retrieve event. State, which will re - done
-commonPatchFuntions.redoToState(patchConfig)
 
-//// Artefacts are tagged = ready to be built and deployed with start of Patch Pipeline
-patchfunctions.stage(target,"Installationsbereit",patchConfig,"Build", patchBuildFunctions.&buildServicesConcurrent)
+pipeline {
+	options {
+		preserveStashes(buildCount: 2)
+		timestamps()
+	}
+
+	agent any
+
+	stages {
+		stage("Entwicklung") {
+			steps {
+				println "This is the entwicklung stage"
+			}
+		}
+		stage("Informatiktest") {
+			steps {
+				println "This is the entwicklung stage"
+			}
+		}
+	}
+}
+
