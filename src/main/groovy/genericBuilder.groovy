a2a4e39c25b1672a2a80ddfb7fa2f0e156ad8262
Christoph Henrici <chhenrici@gmail.com>
2020-10-12 16:36:43 +0200

    Generic Pipeline Job Builder for Assemble, Deploy, Install and Clone

---
+++ src/main/groovy/genericBuilder.groovy
@@ -0,0 +1,24 @@
+def now = new Date()
+timeAsString = now.format("yyyyMMdd-HHmmss")
+def jobName = "${jobPreFix}-${target}-${timeAsString}"
+pipelineJob (jobName) {
+	concurrentBuild(false)
+	definition {
+		cpsScm {
+			lightweight(true)
+			scm {
+				github("${github_repo}","${github_repo_branch}")
+			}
+			scriptPath("${script_path}")
+		}
+	}
+	logRotator(3653,10,3653,-1) // ten years legal retention period
+	description("Pipeline for ${jobPreFix}")
+	parameters {
+		stringParam('TARGET',"${target}", "Target for ${jobPreFix} Pipeline ")
+		stringParam('PARAMETER', "${parameter}", "JSON Parameter for ${jobPreFix} Pipeline")
+	}
+	properties {
+	}
+}
+queue(jobName)
