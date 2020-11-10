5f57237ad254ebfddc92a5ab9d423ce44b5c66ee
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-11-10 07:46:17 +0100

    new parameter for assembleAndDeploy

--- src/main/groovy/assembleAndDeployPipeline.groovy
+++ src/main/groovy/assembleAndDeployPipeline.groovy
@@ -2,13 +2,14 @@ pipeline {
     agent any
 
     parameters {
-        string(name: 'jsonParam', description: 'JSON String containing all required info')
+        string(name: 'TARGET')
+        string(name: 'PARAMETER', description: 'JSON String containing all required info')
     }
 
     stages {
         stage("test") {
             steps {
-                println "This is a test where jsonParam = ${params.jsonParam}"
+                println "This is a test where TARGET = ${params.TARGET} and PARAMETER = ${params.PARAMETER}"
             }
         }
     }
