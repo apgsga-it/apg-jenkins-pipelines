54e838ad412f685076e05573f4d5d91dfd5f6959
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-12-09 11:42:03 +0100

    parameter back as string

--- src/main/groovy/assembleAndDeployPipeline.groovy
+++ src/main/groovy/assembleAndDeployPipeline.groovy
@@ -8,7 +8,7 @@ pipeline {
     parameters {
         //TODO JHE (09.12.2020) : couldn't we provide the target only as part of the PARAMETER params?
         string(name: 'TARGET')
-        text(name: 'PARAMETER', description: 'JSON String containing all required info')
+        string(name: 'PARAMETER', description: 'JSON String containing all required info')
 
     }
 
