b658889501d520e8335d3969e00d51affd6c0146
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-12-14 13:46:33 +0100

    JSON param as global variable for the pipeline

--- src/main/groovy/assembleAndDeployPipeline.groovy
+++ src/main/groovy/assembleAndDeployPipeline.groovy
@@ -9,13 +9,13 @@ pipeline {
         //TODO JHE (09.12.2020) : couldn't we provide the target only as part of the PARAMETER params? Waiting on IT-36505
         string(name: 'TARGET')
         string(name: 'PARAMETER', description: 'JSON String containing all required info')
+    }
 
+    script {
+        env.paramsAsJson = new JsonSlurperClassic().parseText(params.PARAMETER)
     }
 
     stages {
-
-        def paramsAsJson = new JsonSlurperClassic().parseText(params.PARAMETER)
-
         // TODO JHE (14.12.2020): This will probably be done differently, or maybe not necessary as soon as IT-36715 will be done
         stage("Copy Revision file") {
             steps {
@@ -40,7 +40,7 @@ pipeline {
                 script {
                     commonPatchFunctions.log("assembleAndDeploy Job will be started for ${params.target} with following parameter ${params.PARAMETER}")
                     commonPatchFunctions.log("paramAsJson = ${paramsAsJson}")
-                    assembleAndDeployPatchFunctions.assembleAndDeploy(params.TARGET, paramsAsJson)
+                    assembleAndDeployPatchFunctions.assembleAndDeploy(params.TARGET, env.paramsAsJson)
                 }
             }
         }
@@ -48,14 +48,14 @@ pipeline {
     post {
         success {
             script {
-                paramsAsJson.patches.each{patchNumber ->
+                env.paramsAsJson.patches.each{patchNumber ->
                     commonPatchFunctions.notifyDb(patchNumber,"assembleAndDeploy",params.PARAMETER.successNotification,null)
                 }
             }
         }
         unsuccessful {
             script {
-                paramsAsJson.patches.each{patchNumber ->
+                env.paramsAsJson.patches.each{patchNumber ->
                     commonPatchFunctions.notifyDb(patchNumber,"assembleAndDeploy",null,params.PARAMETER.errorNotification)
                 }
             }
