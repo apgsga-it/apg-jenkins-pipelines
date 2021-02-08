02606b46b41f3972b49af96dc85c6098f745bb7f
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-08 10:52:40 +0100

    work in progress

--- src/main/groovy/onClone.groovy
+++ src/main/groovy/onClone.groovy
@@ -16,7 +16,7 @@ pipeline {
 		stage("Pre-process verification") {
 			steps {
 				script {
-					if(target.equalsIgnoreCase("chpi211")) {
+					if(target.equalsIgnoreCase("dchpi211")) {
 						error("Target cannot be production environment !!")
 					}
 
