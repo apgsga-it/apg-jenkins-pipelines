725c0f8f29abe6c0076671b4f0c57ec87faa4864
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-09 16:58:35 +0100

    need to explicitely set the target

--- src/main/groovy/onClone.groovy
+++ src/main/groovy/onClone.groovy
@@ -36,6 +36,7 @@ pipeline {
 						def dateInfo = new SimpleDateFormat("yyyyMMdd_HHmmss_S").format(new Date())
 						def revisionClonedPath = "${env.GRADLE_USER_HOME_PATH}/onclone_${paramsAsJson.target}_patch${p.patchNumber}_${dateInfo}"
 						commonPatchFunctions.createFolder(revisionClonedPath)
+						p.target = paramsAsJson.target
 						parallel(
 								"db-build": {
 										patchfunctions.patchBuildDbZip(p)
