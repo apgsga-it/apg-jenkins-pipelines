331cd6fe03da29382d75d8bdc2dda412d53e5fd7
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-11 11:34:39 +0100

    global lock test

--- src/main/groovy/installPipeline.groovy
+++ src/main/groovy/installPipeline.groovy
@@ -5,61 +5,61 @@ import groovy.json.JsonSlurperClassic
 def paramsAsJson = new JsonSlurperClassic().parseText(params.PARAMETER)
 
 pipeline {
-    agent any
 
-    options {
-        lock resource: "assembleDeployInstall_${paramsAsJson.target}"
-    }
+    lock("test") {
+        agent any
 
-    parameters {
-        string(name: 'PARAMETER', description: 'JSON String containing all required info')
-    }
 
-    stages {
-        //JHE (14.12.2020): This is not really a stage ... but Jenkins won't accept to have step done before parallel tasks (below)
-        stage("Starting logged") {
-            steps {
+        parameters {
+            string(name: 'PARAMETER', description: 'JSON String containing all required info')
+        }
+
+        stages {
+            //JHE (14.12.2020): This is not really a stage ... but Jenkins won't accept to have step done before parallel tasks (below)
+            stage("Starting logged") {
+                steps {
+                    script {
+                        commonPatchFunctions.log("Pipeline started with following parameter : ${paramsAsJson}")
+                        installPatchFunctions.logPatchActivity(paramsAsJson.patchNumbers, paramsAsJson.target, "Started")
+                    }
+                }
+            }
+
+            // JHE (09.12.2020): Seems not easily possible to use variable in stage name for declarative pipeline : https://issues.jenkins.io/browse/JENKINS-43820
+            stage("Install") {
+                steps {
+                    parallel(
+                            "db-install": {
+                                script {
+                                    installPatchFunctions.installDb(paramsAsJson)
+                                }
+                            },
+                            "java-install": {
+                                script {
+                                    installPatchFunctions.installJavaServices(paramsAsJson)
+                                }
+                            }
+                    )
+                }
+            }
+        }
+        post {
+            success {
                 script {
-                    commonPatchFunctions.log("Pipeline started with following parameter : ${paramsAsJson}")
-                    installPatchFunctions.logPatchActivity(paramsAsJson.patchNumbers, paramsAsJson.target, "Started")
+                    installPatchFunctions.logPatchActivity(paramsAsJson.patchNumbers, paramsAsJson.target, "Done")
+                    paramsAsJson.patchNumbers.each { patchNumber ->
+                        commonPatchFunctions.notifyDb(patchNumber, "install", paramsAsJson.successNotification, null)
+                    }
                 }
             }
-        }
+            unsuccessful {
+                script {
+                    paramsAsJson.patchNumbers.each { patchNumber ->
+                        commonPatchFunctions.notifyDb(patchNumber, "install", null, paramsAsJson.errorNotification)
+                    }
+                }
+            }
 
-        // JHE (09.12.2020): Seems not easily possible to use variable in stage name for declarative pipeline : https://issues.jenkins.io/browse/JENKINS-43820
-        stage("Install") {
-            steps {
-                parallel(
-                        "db-install": {
-                            script {
-                                installPatchFunctions.installDb(paramsAsJson)
-                            }
-                        },
-                        "java-install": {
-                            script {
-                                installPatchFunctions.installJavaServices(paramsAsJson)
-                            }
-                        }
-                )
-            }
         }
-    }
-    post {
-        success {
-            script {
-                installPatchFunctions.logPatchActivity(paramsAsJson.patchNumbers, paramsAsJson.target, "Done")
-                paramsAsJson.patchNumbers.each{patchNumber ->
-                    commonPatchFunctions.notifyDb(patchNumber,"install",paramsAsJson.successNotification,null)
-                }
-            }
-        }
-        unsuccessful {
-            script {
-                paramsAsJson.patchNumbers.each{patchNumber ->
-                    commonPatchFunctions.notifyDb(patchNumber,"install",null,paramsAsJson.errorNotification)
-                }
-            }
-        }
-
     }
 }
