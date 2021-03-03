b2703d5cf8e40d30d505c4f4d18d68be1fbe94a7
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-03-03 09:18:47 +0100

    manually adding a patchNumbers parameter for onDemand

--- src/main/groovy/patchOnDemandPipeline.groovy
+++ src/main/groovy/patchOnDemandPipeline.groovy
@@ -4,8 +4,9 @@ import groovy.json.JsonSlurperClassic
 import java.text.SimpleDateFormat
 
 def paramsAsJson = new JsonSlurperClassic().parseText(params.PARAMETERS)
+paramsAsJson.patchNumbers = [paramsAsJson.patchNumber] // For onDemand, we only have one parameter.However, assembleAndDeploy and install are dealing with a list
 def dateInfo = new SimpleDateFormat("yyyyMMdd_HHmmss_S").format(new Date())
-def revisionClonedPath = "${env.GRADLE_USER_HOME_PATH}/patch${paramsAsJson.patchNumbers}_${paramsAsJson.target}_${dateInfo}"
+def revisionClonedPath = "${env.GRADLE_USER_HOME_PATH}/patch${paramsAsJson.patchNumber}_${paramsAsJson.target}_${dateInfo}"
 
 pipeline {
 	parameters {
