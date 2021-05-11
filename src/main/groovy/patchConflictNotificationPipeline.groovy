d4d3efc334470486bba8b5dc43d8dca37b4f0740
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-05-11 09:48:39 +0200

    first version ready to be tested

--- src/main/groovy/patchConflictNotificationPipeline.groovy
+++ src/main/groovy/patchConflictNotificationPipeline.groovy
@@ -4,22 +4,59 @@ import groovy.json.JsonSlurperClassic
 def paramsAsJson = new JsonSlurperClassic().parseText(params.PARAMETERS)
 
 pipeline {
-    parameters {
-        string(name: 'PARAMETERS', description: "JSON parameters")
-    }
-
     agent any
 
     stages {
-        stage("SendMail") {
+        stage('Hello') {
             steps {
                 script {
+                    paramsAsJson.each{conflict ->
+                        def p1Number = conflict.patchConflict.p1.patchNumber
+                        def p2Number = conflict.patchConflict.p2.patchNumber
+                        def dockerServices = conflict.patchConflict.dockerServices
+                        def dbObjects = conflict.patchConflict.dbObjects
+                        def javaArtifacts = conflict.patchConflict.serviceWithMavenArtifacts
+                        def body = "Conflict(s) between Patch ${p1Number} and ${p2Number}. Following objects are present in both patches:"
+                        body += System.getProperty('line.separator')
+                        body += System.getProperty('line.separator')
+                        if(!dockerServices.isEmpty()) {
+                            body += "Docker Service(s)"
+                            body += System.getProperty('line.separator')
+                            body += "================="
+                            body += System.getProperty('line.separator')
+                            body += dockerServices.join(",")
+                        }
+                        if(!dbObjects.isEmpty()) {
+                            body += System.getProperty('line.separator')
+                            body += System.getProperty('line.separator')
+                            body += "Db Object(s)"
+                            body += System.getProperty('line.separator')
+                            body += "============"
+                            dbObjects.each{dbo ->
+                                body += System.getProperty('line.separator')
+                                body += "Module Name: ${dbo.moduleName} // Path: ${dbo.filePath} // Object name: ${dbo.fileName}"
+                            }
+                        }
+                        if(!javaArtifacts.isEmpty()) {
+                            body += System.getProperty('line.separator')
+                            body += System.getProperty('line.separator')
+                            body += "Java Artifact(s)"
+                            body += System.getProperty('line.separator')
+                            body += "================"
+                            for(def service : javaArtifacts.keySet()) {
+                                body += System.getProperty('line.separator')
+                                body += "Artifact(s) for service ${service} :"
+                                javaArtifacts.get(service).each{art ->
+                                    body += System.getProperty('line.separator')
+                                    body += " - Artifact Id: ${art.artifactId} // Group Id: ${art.groupId} // Version: ${art.version}"
+                                }
+                            }
+                        }
 
-                    emailext to: "Julien.Helbling@apgsga.ch", subject: "Test patch conflict", body: "this is only a test with hardcoded values ...."
-                    emailext to: "helblingjulien@gmail.com", subject: "Test patch conflict", body: "this is only a test with hardcoded values ...."
+                        emailext to: conflict.emailAdress.join(";"), from: "it.architektur@apgsga.ch" , subject: "Patch Conflict(s)", body: "${body}"
+                    }
                 }
             }
         }
-
     }
 }
